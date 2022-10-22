d70a1fae39aa708f0b91aaf5cc68b231bd6e0f26
Jesse <jesseramos6337@gmail.com>
2022-10-22 09:14:15 -0400

    synthetic division is done

--- longDivision.py
+++ longDivision.py
@@ -1,7 +1,4 @@
-import math
-
-
-coefficents = [1,-5,-22,56]
+coefficents = [1,-4,-15,18]
 lasti = len(coefficents)-1
 
 constant = coefficents[lasti]
@@ -10,17 +7,20 @@ for i in range(1,constant):
     if (constant/i).is_integer():
         possibleFactors.append(i)
 sum = 1
-for i in range(len(possibleFactors)):
-    factor = possibleFactors[i]
-    previous = 0
-    sumlist = []
-    for j in range(len(coefficents)):
-        sumlist.append(coefficents[j] + previous* factor)
-        previous = sumlist[j]
-    if sumlist[len(sumlist)-1] == 0:
-        finalFactor = factor
-        break
-print(finalFactor)
-        
-
-
+def longD(possibleFactorsD):
+    for i in range(len(possibleFactors)):
+        factor = possibleFactorsD[i]
+        previous = 0
+        sumlist = []
+        for j in range(len(coefficents)):
+            sumlist.append(coefficents[j] + previous* factor)
+            previous = sumlist[j]
+        if sumlist[len(sumlist)-1] == 0:
+            finalFactor = factor
+            print(sumlist)
+            return finalFactor
+print("First positive factor(x-{})".format(longD(possibleFactors)))
+negativePossitiveFactors = []
+for i in possibleFactors:
+    negativePossitiveFactors.append(i* -1)  
+print("FIrst negative factor(x-{})".format(longD(negativePossitiveFactors)))
